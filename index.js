ec73595512b16747f9721d6f331bba33cb315407
Aaron Johnson <aaronjohnson@Pauls-MBP.lan>
2024-01-15 11:34:06 -0800

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+
+function appendCat(name) {
+    const  newCats = [...cats, name]
+    return newCats;
+}
+
+function prependCat(name) {
+    const  newCats = [name, ...cats]
+    return newCats
+}
+
+function removeLastCat() {
+    const copyOfNewCats = cats.slice()
+    copyOfNewCats.pop()
+    return copyOfNewCats
+}
+
+function removeFirstCat() {
+    const copyOfFirstCats = cats.slice()
+    copyOfFirstCats.shift()
+    return copyOfFirstCats
+}
